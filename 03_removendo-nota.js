4632f69b54a5899f25228355a557484c5fc6dbeb
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 09:38:45 -0300

    Feat: implement basic test with pop() function of arrays in 03_removendo-nota.js

---
+++ 03_removendo-nota.js
@@ -0,0 +1,8 @@
+// usando o pop() para remover o último elemento do array 'notas'
+const notas = [10, 6, 8, 10];
+notas.pop();
+console.log(notas);
+
+media = (notas[0] + notas[1] + notas[2]) / notas.length;
+
+console.log(media);
