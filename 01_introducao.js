96b2fd67ffc3481e048465adbb756b2ad5794ace
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 09:20:40 -0300

    Feat: implement raw average of notes without arrays in 01_introducao.js

---
+++ 01_introducao.js
@@ -0,0 +1,10 @@
+// desafio: calcular média das notas 10, 6.5, 8 e 7.5
+
+let primeiraNota = 10;
+let segundaNota = 6.5;
+let terceiraNota = 8;
+let quartaNota = 7.5;
+
+let media = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4;
+
+console.log(`A média das notas é: ${media}`);
