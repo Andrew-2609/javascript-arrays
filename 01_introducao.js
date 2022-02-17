c60ee0264e96972b166698ab8450cbf4f8474cf7
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 09:29:48 -0300

    Feat: use some array features to calculate the result in 01_introducao.js

--- 01_introducao.js
+++ 01_introducao.js
@@ -1,10 +1,14 @@
 // desafio: calcular média das notas 10, 6.5, 8 e 7.5
 
-let primeiraNota = 10;
-let segundaNota = 6.5;
-let terceiraNota = 8;
-let quartaNota = 7.5;
+// let primeiraNota = 10;
+// let segundaNota = 6.5;
+// let terceiraNota = 8;
+// let quartaNota = 7.5;
 
-let media = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4;
+// let media = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4;
+
+const notas = [10, 6.5, 8, 7.5];
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(`A média das notas é: ${media}`);
