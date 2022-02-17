bee80d9cc90a6b9ca0f35dfbb7790daba3fac0b5
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-17 09:36:34 -0300

    Feat: implement basic test with push() function of arrays in 02_adicionando-nota.js

---
+++ 02_adicionando-nota.js
@@ -0,0 +1,9 @@
+// adicionando novo elemento ao array
+const notas = [10, 6, 8]; // , 7
+notas.push(7);
+console.log(notas);
+
+// calculando a média
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media);
