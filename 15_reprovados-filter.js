00adfe9759f22a91c36c0a163d28693b56ec36e3
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-22 22:40:41 -0300

    Refactor: change name of first parameter of filter() function to '_' in 15_reprovados-filter.js

--- 15_reprovados-filter.js
+++ 15_reprovados-filter.js
@@ -1,7 +1,7 @@
 const nomes = ['Ana', 'Marcos', 'Maria', 'Mauro'];
 const notas = [7, 4.5, 9, 7.5];
 
-const reprovados = nomes.filter((aluno, indice) => {
+const reprovados = nomes.filter((_, indice) => {
     return notas[indice] < 7;
 });
 
