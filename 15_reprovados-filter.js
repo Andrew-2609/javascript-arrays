e909c9b9373f871c31a82033b0ca8cd5e63511d2
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-22 22:39:38 -0300

    Feat: implement basic example using filter() function in 15_reprovados-filter.js

---
+++ 15_reprovados-filter.js
@@ -0,0 +1,8 @@
+const nomes = ['Ana', 'Marcos', 'Maria', 'Mauro'];
+const notas = [7, 4.5, 9, 7.5];
+
+const reprovados = nomes.filter((aluno, indice) => {
+    return notas[indice] < 7;
+});
+
+console.log(`Lista de alunos: ${nomes}.\nAlunos reprovados: ${reprovados}`);
