b46172c36d859ef00a5b8a0c9b628dd6b9e544fb
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-02-22 21:11:42 -0300

    Feat: implement a basic test using bidimensional arrays in 07_lista-duas-dimensoes.js

---
+++ 07_lista-duas-dimensoes.js
@@ -0,0 +1,6 @@
+const alunos = ['João', 'Juliano', 'Caio', 'Alana'];
+const mediasDosAlunos = [10, 7, 9, 6];
+
+const listaDeNotasEAlunos = [alunos, mediasDosAlunos];
+
+console.log(`${listaDeNotasEAlunos[0][0]}, sua média foi de: ${listaDeNotasEAlunos[1][0]}`);
